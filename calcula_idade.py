c816c9231782a7c3b5a87449c11df16ab61d347c
ryansgp123 <ryansgp02@gmail.com>
2025-09-16 08:50:36 -0300

    Add files via upload

---
+++ calcula_idade.py
@@ -0,0 +1,6 @@
+ano_nascimento = int(input("Digite o seu ano de nascimento: "))
+ano_atual = int(input("Digite o ano atual: "))
+
+idade = ano_atual - ano_nascimento
+
+print(f"Você tem {idade} anos.")
